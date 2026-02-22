79391e564501840ce18bf0c0ac43e1afe90bd759
ibrohim3 <ibrohim.2128382@gmail.com>
2026-02-22 18:00:02 +0500

    add

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,24 +1,9 @@
-// import axios from "axios"
-
-// const API = axios.create({
-//     // baseURL: "http://localhost:5000/api"
-//     baseURL: "https://exceptional-nonoperable-latoyia.ngrok-free.dev/api"
-// })
-
-// API.interceptors.request.use((config) => {
-//     const token = localStorage.getItem("token")
-//     if (token) config.headers.Authorization = `Bearer ${token}`
-//     return config
-// })
-
-// export default API
 
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api",
-    // baseURL: "https://exceptional-nonoperable-latoyia.ngrok-free.dev/api"
-
+    // baseURL: "http://localhost:5000/api",
+    baseURL: "https://mini-warehouse-system-production.up.railway.app/api"
 });
 
 API.interceptors.request.use(
